9b3d2a7851b516839b4cee18458dff26b5740ebb
AndreLuis6 <handreluis.369@gmail.com>
2023-01-25 23:21:50 -0300

    Adicionar vagas

---
+++ sistema/app/Http/Controllers/VagaController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Vaga;
+
+class VagaController extends Controller
+{
+    public function store(Request $request) {
+        $vaga = new Vaga;
+        
+        $vaga->nome = $request->nome;
+        $vaga->endereco = $request->endereco;
+        $vaga->numero = $request->numero;
+        $vaga->bairro = $request->bairro;
+        $vaga->cidade = $request->cidade;
+        $vaga->estado = $request->estado;
+
+        $vaga->referencia = $request->referencia;
+        $vaga->descricao = $request->descricao;
+        $vaga->telefone = $request->telefone;
+        $vaga->email = $request->email;
+        $vaga->valor = $request->valor;
+
+        $vaga->qtd_homem = $request->qtd_homem;
+        $vaga->qtd_mulher = $request->qtd_mulher;
+
+        $vaga->save();
+
+        return redirect('/vagas');
+    }
+}
---
+++ sistema/database/migrations/2023_01_24_022754_create_vagas_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vagas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("nome");
+            $table->string("endereco");
+            $table->integer("numero");
+            $table->string("bairro");
+            $table->string("cidade");
+            $table->string("estado");
+            $table->string("referencia");
+            $table->string("descricao");
+            $table->string("telefone");
+            $table->string("email");
+            $table->string("valor");
+            $table->integer("qtd_homem");
+            $table->integer("qtd_mulher");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vagas');
+    }
+};
