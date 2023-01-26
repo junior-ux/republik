1ddf5341b13a7d5a385fad9c895048f3a59ab69e
AndreLuis6 <handreluis.369@gmail.com>
2023-01-25 23:21:26 -0300

    Adicionar pessoas

---
+++ sistema/app/Http/Controllers/PessoaController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Pessoa;
+
+class PessoaController extends Controller
+{
+    public function store(Request $request) {
+        $pessoa = new Pessoa;
+        
+        $pessoa->nome = $request->nome;
+        $pessoa->telefone = $request->telefone;
+        $pessoa->email = $request->email;
+        $pessoa->sobre = $request->sobre;
+        $pessoa->nascimento = $request->nascimento;
+        $pessoa->instagram = $request->instagram;
+
+        $pessoa->save();
+
+        return redirect('/pessoas');
+    }
+}
---
+++ sistema/database/migrations/2023_01_26_013112_create_pessoas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pessoas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("nome");
+            $table->string("telefone");
+            $table->string("email");
+            $table->string("sobre");
+            $table->date('nascimento')->nullable();
+            $table->string("instagram");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pessoas');
+    }
+};
