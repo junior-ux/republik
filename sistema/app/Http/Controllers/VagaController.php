b9e3efdf400460b255b2a2409f349e2cec754060
AndreLuis6 <handreluis.369@gmail.com>
2023-02-03 00:54:57 -0300

    Metodos de editar e remover

--- sistema/app/Http/Controllers/VagaController.php
+++ sistema/app/Http/Controllers/VagaController.php
@@ -36,6 +36,9 @@ class VagaController extends Controller
             $vaga->image = $imageName;
         }
 
+        $user = auth()->user();
+        $vaga->user_id = $user->id;
+
         $vaga->save();
 
         return redirect('/vagas');
@@ -46,4 +49,44 @@ class VagaController extends Controller
 
         return view('ver-mais-vagas', ['vagas' => $vagas]);
     }
+
+    public function perfil() {
+
+        $user = auth()->user();
+        $vagas = $user->vagas;
+
+        return view('perfil', ['vagas' => $vagas]);
+    }
+
+    public function destroy($id) {
+        Vaga::findOrFail($id)->delete();
+
+        return redirect('/perfil');
+    }
+
+    public function update(Request $request){
+        $vaga = Vaga::findOrFail($request->id);
+        echo $vaga;
+
+        $vaga->nome = $request->nome;
+        $vaga->endereco = $request->endereco;
+        $vaga->numero = $request->numero;
+        $vaga->bairro = $request->bairro;
+        $vaga->cidade = $request->cidade;
+        $vaga->estado = $request->estado;
+
+        $vaga->referencia = $request->referencia;
+        $vaga->descricao = $request->descricao;
+        $vaga->telefone = $request->telefone;
+        $vaga->email = $request->email;
+        $vaga->valor = $request->valor;
+
+        $vaga->qtd_homem = $request->qtd_homem;
+        $vaga->qtd_mulher = $request->qtd_mulher;
+
+        $vaga->save();
+
+        return redirect('/perfil');
+    }
+
 }
